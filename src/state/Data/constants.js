a443b2361d7f0e2fc4c7a89dc54d6d07024622d6
Vojtěch Dubrovský <v.dubr@hotmail.com>
2021-12-21 16:27:11 +0100

    Load missing attributes from dataEndpoint if DS is type vector

--- src/state/Data/constants.js
+++ src/state/Data/constants.js
@@ -2,3 +2,4 @@
 // Tiled vectors has spatial and attributes indexes with loading indication.
 // TODO 'vector' is only temporary, remove it once it is modified on BE
 export const TILED_VECTOR_LAYER_TYPES = ['tiledVector', 'tiled-vector'];
+export const SINGLE_VECTOR_LAYER_TYPES = ['vector'];
