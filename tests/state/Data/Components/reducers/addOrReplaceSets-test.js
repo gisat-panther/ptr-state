3ec3c4efbb95b82bb48fad0eca11408ddad51541
PavelVlach <vlach1989@gmail.com>
2022-06-13 14:03:48 +0200

    Component sets addition

---
+++ tests/state/Data/Components/reducers/addOrReplaceSets-test.js
@@ -0,0 +1,57 @@
+import {assert} from 'chai';
+import reducers, {
+	INITIAL_STATE,
+} from '../../../../../src/state/Data/Components/reducers';
+
+describe('addOrReplaceSets-test', function () {
+	const state = {
+		...INITIAL_STATE,
+		sets: {
+			...INITIAL_STATE.sets,
+			byKey: {
+				setA: {
+					components: ['componentA']
+				},
+			},
+		},
+	};
+
+	it('Should add set to store', function () {
+		const expectedState = {
+			...state,
+			sets: {
+				...state.sets,
+				byKey: {
+					...state.sets.byKey,
+					setA: {
+						components: ['componentA']
+					},
+				},
+			},
+		};
+
+		const action = {
+			type: 'DATA.COMPONENTS.ADD_SETS',
+			sets: {
+				setA: {
+					components: ['componentA']
+				},
+			},
+		};
+
+		const output = reducers(state, action);
+
+		assert.deepStrictEqual(output, expectedState);
+	});
+
+	it('Should return the same state if no sets was given', function () {
+		const action = {
+			type: 'DATA.COMPONENTS.ADD_SETS',
+			sets: null,
+		};
+
+		const output = reducers(state, action);
+
+		assert.equal(output, state);
+	});
+});
